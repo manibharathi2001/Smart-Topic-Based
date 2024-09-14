9ae048e9176f103944830d1dc216da3b94a64c95
Mani bharathi <128881256+manibharathi2001@users.noreply.github.com>
2024-09-14 23:45:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def index():
     return render_template('index.html', facts=facts, topic=topic, error=error, recent_chats=recent_chats)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
